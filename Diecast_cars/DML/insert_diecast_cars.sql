5e9cee48e277d5ffc4b5304ceff0cc5676b4358b
u2cc <cheng.james.chen@gmail.com>
2022-04-20 21:50:05 -0400

    Revert "Add implementation history up to current progress"
    This reverts commit 20aa1f80d9d2b068a9fa98be72e47cc193348c2d.

--- Diecast_cars/DML/insert_diecast_cars.sql
+++ Diecast_cars/DML/insert_diecast_cars.sql
@@ -1,10 +1,5 @@
 
 use diecast_management_db;
 select* from diecast_cars;
-INSERT INTO diecast_cars VALUES (NULL, 'Hot Wheels', 'Premiums', 'Nissan','''82 Skyline R30', 64.0, 'red','coupe', '2019-04-10','Walmart Markville');
-INSERT INTO diecast_cars VALUES (NULL, 'Matchbox', 'Collectors', 'Land Rover','Defender 110-1997', 64.0, 'orange','Off-roader', '2019-04-11','Walmart Markville');
-
-
-truncate diecast_cars;
-
+INSERT INTO diecast_cars VALUES (NULL, 'Hot Wheels', 'Premiums', 'Nissasn','''82 Skyline R30', 64.0, 'red','coupe', '2019-04-10','Walmart Markville');
 commit;
